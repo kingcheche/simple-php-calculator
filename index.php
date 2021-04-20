c5a48e2b91aa97f5f5ab5284300a61d5022d42c9
Cheche Kalu <kalugreymatter@gmail.com>
2021-04-20 20:46:12 +0100

    Real Calculator simulation create with php - commented

--- index.php
+++ index.php
@@ -5,63 +5,72 @@
 <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-   <!-- <div class="relaod">
-<form  name="relaod" action="index.php" method="POST">
-   <input type="submit" value="Force reload">
-</from>
-</div> -->
 <div class="container">
+<!-- Calculator Display -->
 <fieldset>
 <Legend> Calculator</legend>
+<!-- Using method=GET so the form input will not be saved on reload -->
 <form name="cal" method="GET">
 <?php
-
+//Here is where the magic begins
 if(isset($_GET["enter"]))
 {
-
+// Check if display is empty - and if its empty show an error
    if (empty($_GET["display"])) {
       echo "<span class='error'> Input Error:Clr <span class='c'> C  </span> </span> ";
       echo  "<hr>";
       
    } else {
-      
+//Convert the display inputs to a new variable for ease and perform futher checks      
       $ma =$_GET["display"];      
       switch ($ma) {
+
+         //check if the inputs starts with a "/" and display and error
             case (str_starts_with($ma, '/')):
            echo "<span class='error'> Syntax Error:Clr <span class='c'> C  </span> </span> ";
            echo  "<hr>";
             break;
+            //check if the inputs starts with a "*" and display and error
             case (str_starts_with($ma, '*')):
            echo  "<span class='error'> Syntax Error:Clr <span class='c'> C  </span> </span> ";
            echo  "<hr>";
             break;
+            //check if the inputs ends with a "/" and display and error
             case (str_ends_with($ma, '/')):
            echo  "<span class='error'> Syntax Error:Clr <span class='c'> C  </span> </span> ";
            echo  "<hr>";
             break;
+            //check if the inputs ends with a "*" and display and error
             case (str_ends_with($ma, '*')):
             echo  "<span class='error'> Syntax Error:Clr <span class='c'> C  </span> </span> ";
             echo  "<hr>";
                break;
+            //check if the inputs ends with a "-" and display and error
                case (str_ends_with($ma, '-')):
             echo  "<span class='error'> Syntax Error:Clr <span class='c'> C  </span> </span> ";
             echo  "<hr>";
                break;
+            //check if the inputs ends with a "+" and display and error
                case (str_ends_with($ma, '+')):
             echo  "<span class='error'> Syntax Error:Clr <span class='c'> C  </span> </span> ";
             echo  "<hr>";
                break;
+            //If all the validation is passed process the answer
                default: 
+            //Because the input is a string, convert to inter and evaluate   
                $answer = eval('return '.$ma.';');
+            //Save and display the question   
               $question = $_GET["display"];
             echo $question;
             echo "<hr>";
+            //Display process answer
             echo ("<span class='answer'>$answer</span>");
    }
 }
 }
  else 
  {
+    //Show empty form on default page load
    $displayForm ="<input type = 'text'  name='display' class='display' maxlength = '11'>";
    echo $displayForm;
    echo "<hr>";
@@ -70,6 +79,7 @@ if(isset($_GET["enter"]))
 ?>
 </fieldset>
 <br>
+<!-- Form that send its value to display when you click on it -->
 <input type="button" value="7" class="num" onclick="cal.display.value+='7' ">
 <input type="button" value="8" class="num" onclick="cal.display.value+='8' ">
 <input type="button" value="9" class="num" onclick="cal.display.value+='9' ">
